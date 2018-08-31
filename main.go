9df96c2cf35ece7ca37e8fd3014095af03b318ea
reader@sewan.fr <robot@robot-ci.sewan>
2018-08-31 10:10:20 +0000

    Gitlab "release-process" pipeline job, update import path to match github ones and rm pipeline files.

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"github.com/hashicorp/terraform/plugin"
 	"github.com/hashicorp/terraform/terraform"
-	"gitlab.com/rd/terraform-provider-sewan/sewan"
+	"github.com/SewanDevs/terraform-provider-sewan/sewan"
 )
 
 func main() {
